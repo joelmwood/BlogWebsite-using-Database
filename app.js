af25bb5d39b61a7afdca61feabb400cbf871a39c
Joel <joelmwood@live.com>
2021-09-28 03:37:14 -0500

    Attach blog posts to external database to save posts

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://admin-joel:Test123@cluster0.x17z2.mongodb.net/blogsDB", {
+  useNewUrlParser: true
+});
+
+const blogSchema = new mongoose.Schema({
+  blogTitle: String,
+  blogContent: String
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,13 +30,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+//  let posts = [];
 
 app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-    });
+  Blog.find({}, function(err, foundItems){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: foundItems
+      });
+  });
 });
 
 app.get("/about", function(req, res){
@@ -38,33 +54,42 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
+  // const post = {
+  //   title: req.body.postTitle,
+  //   content: req.body.postBody
+  // };
 
-  posts.push(post);
+  const blog = new Blog({
+    blogTitle: req.body.postTitle,
+    blogContent: req.body.postBody
+  });
+  blog.save();
+
+  // posts.push(post);
 
   res.redirect("/");
 
 });
 
 app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+  //const requestedTitle = _.lowerCase(req.params.postName);
+  const requestedTitle = req.params.postName;
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
 
-    if (storedTitle === requestedTitle) {
+  Blog.findOne({blogTitle: requestedTitle}, function(err, foundItem){
+    if(!err){
+      console.log(foundItem);
       res.render("post", {
-        title: post.title,
-        content: post.content
+        title: foundItem.blogTitle,
+        content: foundItem.blogContent
       });
+    } else {
+      console.log(err)
     }
   });
 
 });
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started");
 });
